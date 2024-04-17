c4df415a686c7166152367637861b540fbc64e32
BubuCarla <124883719+BubuCarla@users.noreply.github.com>
2024-04-17 08:45:25 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.js
+++ JogoDaVelha.js
@@ -1,95 +1,201 @@
-//  tabuleiro
-let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+    public class Jogo {
+
+        private char[][] board = {
+
+                {' ', ' ', ' '},
+
+                {' ', ' ', ' '},
+
+                {' ', ' ', ' '}
+
+        };
 
 
-let player = 'X';
+        private char currentPlayer = 'X';
 
 
-function printBoard() {
-    for (let i = 0; i < 3; i++) {
-        console.log(board[i].join(' | '));
-        if (i < 2) {
-            console.log('---------');
-        }
-    }
-}
+        public void printBoard() {
 
-function checkganhador() {
-   
-    for (let i = 0; i < 3; i++) {
-        if (board[i][0] === player && board[i][1] === player && board[i][2] === player) {
-            return true;
-        }
-    }
+            for (int i = 0; i < 3; i++) {
 
-    for (let j = 0; j < 3; j++) {
-        if (board[0][j] === player && board[1][j] === player && board[2][j] === player) {
-            return true;
-        }
-    }
-    // diagonais
-    if ((board[0][0] === player && board[1][1] === player && board[2][2] === player) ||
-        (board[0][2] === player && board[1][1] === player && board[2][0] === player)) {
-        return true;
-    }
-    return false;
+                for (int j = 0; j < 3; j++) {
 
+                    System.out.print(board[i][j] + " | ");
 
-function checkEmpate() {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[i][j] === '') {
-                return false;
+                }
+
+                System.out.println();
+
+                if (i < 2) {
+
+                    System.out.println("---------");
+
+                }
+
             }
+
         }
-    }
-    return true;
-}
 
 
-function playerMove(row, col) {
-    if (board[row][col] === '') {
-        board[row][col] = currentPlayer;
-        if (checkWin()) {
-            console.log(`Parabéns! Você venceu!`);
-            printBoard();
-            return;
-        } else if (checkDraw()) {
-            console.log('Empate!');
-            printBoard();
-            return;
+        public boolean checkWin() {
+
+            for (int i = 0; i < 3; i++) {
+
+                if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+
+                    return true;
+
+                }
+
+            }
+
+
+            for (int j = 0; j < 3; j++) {
+
+                if (board[0][j] == currentPlayer && board[1][j] == currentPlayer && board[2][j] == currentPlayer) {
+
+                    return true;
+
+                }
+
+            }
+
+
+            if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+
+                return true;
+
+            } else if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+
+                return true;
+
+            }
+
+
+            return false;
+
         }
+
+
+        public boolean checkDraw() {
+
+            for (int i = 0; i < 3; i++) {
+
+                for (int j = 0; j < 3; j++) {
+
+                    if (board[i][j] == ' ') {
+
+                        return false;
+
+                    }
+
+                }
+
+            }
+
+            return true;
+
+        }
+
+
+        public void playerMove(int row, int col) {
+
+            if (board[row][col] == ' ') {
+
+                board[row][col] = currentPlayer;
+
+                if (checkWin()) {
+
+                    System.out.println("Congratulations! You won!");
+
+                    printBoard();
+
+                } else if (checkDraw()) {
+
+                    System.out.println("It's a draw!");
+
+                    printBoard();
+
+                } else {
+
+                    currentPlayer = (currentPlayer == 'X')? 'O' : 'X';
+
+                    System.out.println("It's the computer's turn:");
+
+                    computerMove();
+
+                    printBoard();
+
+                }
+
+            } else {
+
+                System.out.println("That space is already occupied. Choose another one.");
+
+            }
+
+        }
+
+
+        public void computerMove() {
+
+            Random random = new Random();
+
+            int row, col;
+
+            do {
+
+                row = random.nextInt(3);
+
+                col = random.nextInt(3);
+
+            } while (board[row][col]!= ' ');
+
+            board[row][col] = currentPlayer;
+
+            if (checkWin()) {
+
+                System.out.println("The computer won!");
+
+                printBoard();
+
+            } else if (checkDraw()) {
+
+                System.out.println("It's a draw!");
+
+                printBoard();
+
+            } else {
+
+                currentPlayer = (currentPlayer == 'X')? 'O' : 'X';
+
+                System.out.println("It's your turn:");
+
+            }
+
+        }
+
+
+        public void main(String[] args) {
+
+            Jogo game = new Jogo();
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+
+                game.printBoard();
+
+                System.out.println("Enter row and column (1-3):");
+
+                int row = scanner.nextInt() - 1;
+
+                int col = scanner.nextInt() - 1;
+
+                game.playerMove(row, col);
+
+            }
+
+        }
+
     
-        currentPlayer = 'O';
-        console.log("Vez do console:");
-        setTimeout(computerMove, 1000); 
-        printBoard();
-    } else {
-        console.log('Essa posição já está ocupada. Escolha outra.');
-    }
-}
-
-function consoleMove() {
-    let row, col;
-    do {
-        row = Math.floor(Math.random() * 3);
-        col = Math.floor(Math.random() * 3);
-    } while (board[row][col] !== '');
-    board[row][col] = currentPlayer;
-    if (checkWin()) {
-        console.log(`O console venceu!`);
-        printBoard();
-        return;
-    } else if (checkDraw()) {
-        console.log('Empate!');
-        printBoard();
-        return;
-    }
-   
-    player = 'X';
-    console.log("Sua vez:");
-}}
